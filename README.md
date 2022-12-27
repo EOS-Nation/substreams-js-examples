497cde436f0480cb36febd1bec395ac5f389c09a
Denis Carriere <carriere.denis@gmail.com>
2022-12-26 20:44:12 -0500

    update examples

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# substreams-nodejs-examples
-Substreams NodeJS examples
+# Substreams Examples
+
+## Quickstart
+
+```bash
+$ npm install
+$ node index.js
+
+File already exists: QmUc8qGvJ8rVsTQV6L2pvZEEwpfw3K6LxcxX9FMvF4cPB4
+ActionTrace {
+  blockNum: 283000005,
+  trxId: 'ee944bf8bc0591291a93c88cb7113f3196cda021b9c65a62bfe9c0bbf447d674',
+  actionOrdinal: 3,
+  account: 'prospectorsg',
+  receiver: 'prospectorsg',
+  name: 'transfer',
+  jsonData: '{"from":"prospectorst","memo":"migration from wax/qepcpara1234","quantity":"0.6170 PGL","to":"lisrx13t21po"}',
+  timestamp: Timestamp { seconds: 1670679060n, nanos: 0 }
+}
+ActionTrace {
+  blockNum: 283000005,
+  trxId: 'ee944bf8bc0591291a93c88cb7113f3196cda021b9c65a62bfe9c0bbf447d674',
+  actionOrdinal: 4,
+  account: 'prospectorsg',
+  receiver: 'prospectorst',
+  name: 'transfer',
+  jsonData: '{"from":"prospectorst","memo":"migration from wax/qepcpara1234","quantity":"0.6170 PGL","to":"lisrx13t21po"}',
+  timestamp: Timestamp { seconds: 1670679060n, nanos: 0 }
+}
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const { Substreams, download } = require("substreams");
+
+// User input
+const host = "eos.firehose.eosnation.io:9001";
+const substream = "QmUc8qGvJ8rVsTQV6L2pvZEEwpfw3K6LxcxX9FMvF4cPB4";
+const outputModules = ["map_action_traces"];
+const startBlockNum = "283000000";
+const stopBlockNum = "283001000";
+
+// Initialize Substreams
+const substreams = new Substreams(host, {
+    startBlockNum,
+    stopBlockNum,
+    outputModules,
+});
+
+(async () => {
+    // download Substream from IPFS
+    const {modules, registry} = await download(substream);
+    
+    // Find Protobuf message types from registry
+    const ActionTraces = registry.findMessage("antelope.common.v1.ActionTraces");
+    if ( !ActionTraces) throw new Error("Could not find ActionTraces message type");
+
+    substreams.on("mapOutput", output => {
+        const { actionTraces } = ActionTraces.fromBinary(output.data.mapOutput.value);
+        for ( const actionTrace of actionTraces ) {
+            console.log(actionTrace);
+        }
+    });
+
+    // start streaming Substream
+    await substreams.start(modules);
+
+    // end of Substream
+    console.log("done");
+    process.exit();
+})();
